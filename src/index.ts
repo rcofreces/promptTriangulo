f391592806eff81693ba3f6e604e5dd4a3c9656e
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-16 14:34:00 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let base: number = Number(prompt("Indique la base del triángulo"));
+let altura: number = Number(prompt("Indique la altura del triángulo"));
+let area: number = base * altura;
+
+console.log("El área del triángulo es de:", area);
